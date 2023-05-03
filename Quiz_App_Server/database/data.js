15e278f767c6f2049adc7519683f1e29fce8a253
SidraTaranum <71249763+SidraTaranum@users.noreply.github.com>
2023-05-03 20:20:27 +0100

    Add files via upload

---
+++ Quiz_App_Server/database/data.js
@@ -0,0 +1,229 @@
+export default [
+    {
+        id: 1,
+        question : "Javascript is an _______ language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 2,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 3,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 4,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer'
+        ]
+    },
+    {
+        id: 5,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 6,
+        question : "Javascript is an _______ language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 7,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 8,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 9,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer'
+        ]
+    },
+    {
+        id: 10,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 11,
+        question : "Javascript is an _______ language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 12,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 13,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 14,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer'
+        ]
+    },
+    {
+        id: 15,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 16,
+        question : "Javascript is an _______ language",
+        options : [
+            'Object-Oriented',
+            'Object-Based',
+            'Procedural',
+        ]
+    },
+    {
+        id: 17,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 18,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 19,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer'
+        ]
+    },
+    {
+        id: 20,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 21,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 22,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 23,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 24,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    },
+    {
+        id: 25,
+        question : "Which function is used to serialize an object into a JSON string?",
+        options : [
+            'stringify()',
+            'parse()',
+            'convert()',
+        ]
+    }
+];
+
+export const answers = [0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0,0, 1, 2, 1, 0];
